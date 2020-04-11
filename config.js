6223fd10e0fbf1dcc638831ffab0f37154f84b6d
gm <graham.murphy@gmail.com>
2020-04-11 01:12:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'CustomerOps', // Required
+  author: 'Graham Murphy', // Required
+  description: 'Really important content.',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://grahammurphy.org',
+    github: 'https://github.com/gm',
+    twitter: 'https://twitter.com/grahammurphy',
+    linkedin: 'https://ca.linkedin.com/in/grahampmurphy',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://gm.github.io/devblog/',
 };
